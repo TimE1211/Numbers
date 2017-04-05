a7b0542f08f91e1a2e4ccdd756129d23c0641d37
shane nelson <smnelson13@live.com>
2017-04-05 09:52:23 -0400

    Finished numbers Project

---
+++ Numbers/NumbersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NumbersTableViewCell.swift
+//  Numbers
+//
+//  Created by shane nelson on 4/5/17.
+//  Copyright © 2017 Timothy Hang. All rights reserved.
+//
+
+import UIKit
+
+class NumbersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
